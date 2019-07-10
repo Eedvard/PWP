0937fd5bbbc5b1cb890a378dba713094fd557abd
Samuli Helttunen <gwchilos@hotmail.com>
2019-07-10 19:09:58 +0300

    users

---
+++ Soodit/Final_api/mealplan/resources/users.py
@@ -0,0 +1,35 @@
+import json
+import flask
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, Response, request
+from flask_restful import Resource
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(255), nullable=False, unique=True)
+
+
+class Users(Resource):
+
+    def get(self, user):
+        db_user = User.query.filter_by(username=user).first()
+        if db_user is None:
+            return create_error_response(404, "Not Found", "No user was found with the username {}".format(user))
+
+    def put(self, user):
+        pass
+
+    def delete(self, user):
+        pass
+
+    def post(self):
+        pass
+
