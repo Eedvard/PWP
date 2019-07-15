e1cefe027b994e6b3930c5cd46b7ca674c03bc68
Samuli Helttunen <gwchilos@hotmail.com>
2019-07-15 17:57:17 +0300

    put and get methods in user

--- Soodit/Final_api/mealplan/resources/users.py
+++ Soodit/Final_api/mealplan/resources/users.py
@@ -4,7 +4,8 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, Response, request
 from flask_restful import Resource
-from mealplan import utils
+from mealplan import utils, db, models, api
+import datetime
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
@@ -21,13 +22,45 @@ class User(db.Model):
 class Users(Resource):
 
     def get(self, user_id):
-        db_user = User.query.filter_by(id=user_id).first()
+        if request.method != "GET":
+            return utils.RecipeBuilder.create_error_response(405, "Wrong method", "GET method required")
+        db_user = models.User.query.filter_by(id=user_id).first()
         if db_user is None:
-            return utils.RecipeBuilder.create_error_response(404, "Not Found", "No user was found with the username {}".format(user)
-                                                             )
+            return utils.RecipeBuilder.create_error_response(404, "Not Found", "No user was found with the username {}".format(user_id))
 
-    def put(self, user):
-        pass
+        body = utils.RecipeBuilder(
+            username=db_user.username
+        )
+        return Response(json.dumps(body), 200, mimetype=utils.MASON)
+
+    def put(self, user_id):
+        if request.method != "PUT":
+            return utils.RecipeBuilder.create_error_response(405, "Wrong method", "PUT method required")
+        db_user = models.User.query.filter_by(id=user_id).first()
+        if db_user is None:
+            return utils.RecipeBuilder.create_error_response(404, "Not Found", "No user was found with the username {}".format(user_id))
+        try:
+            username = str(request.json["username"])
+        except KeyError:
+            return utils.RecipeBuilder.create_error_response(400, "Missing fields", "Incomplete request - missing fields")
+        except ValueError:
+            return utils.RecipeBuilder.create_error_response(400, "Invalid input", "Username must be a string")
+        except TypeError:
+            return utils.RecipeBuilder.create_error_response(415, "Invalid content", "Request content must be JSON")
+
+        body = utils.RecipeBuilder(
+            username=db_user.username
+        )
+
+        db_user.username = username
+
+        db.session.commit()
+        url = api.api.url_for(User, user_id=user_id)
+        return Response(headers={
+            "Location": url
+        },
+            status=204
+        )
 
     def delete(self, user):
         pass
