5c97965213b0af69c96215214bf73ba98a4bcf6f
Samuli Helttunen <gwchilos@hotmail.com>
2019-07-17 17:37:02 +0300

    tests for control methods (missing POST)

--- Soodit/Final_api/mealplan/resources/resource_test.py
+++ Soodit/Final_api/mealplan/resources/resource_test.py
@@ -35,12 +35,78 @@ def client():
     os.close(db_fd)
     os.unlink(db_fname)
 
-#TESTING THE USER DB
 
-    def _populate_db():
-        for i in range(1, 4):
-            u = Users(
-                username="user-{}".format(i)
-            )
-            db.session.add(u)
-        db.session.commit()
+# TESTING THE USER DB
+
+
+def _populate_db():
+    for i in range(1, 4):
+        u = Users(
+            username="user-{}".format(i)
+        )
+        db.session.add(u)
+    db.session.commit()
+
+
+def _get_user_json(number=1):
+    """
+    Creates a valid user JSON object to be used for PUT and POST tests.
+    """
+    return {"username": "extra-user-{}".format(number)}
+
+
+def _check_namespace(client, response):
+    """
+     Checks that the "profile" namespace is found from the response body, and
+     that its "name" attribute is a URL that can be accessed.
+     """
+
+    ns_href = response["@namespaces"]["profile"]["name"]
+    resp = client.get(ns_href)
+    assert resp.status_code == 200
+
+def _check_control_get_method(ctrl, client, obj)
+    """
+    Checks a GET type control from a JSON object be it root document or an item
+    in a collection. Also checks that the URL of the control can be accessed.
+    """
+
+    href = obj["@controls"][ctrl]["href"]
+    resp = client.get(href)
+    assert resp.status_code == 200
+
+def _check_control_delete_method(ctrl, client, obj)
+    """
+    Checks a DELETE type control from a JSON object be it root document or an
+    item in a collection. Checks the control's method in addition to its "href".
+    Also checks that using the control results in the correct status code of 204.
+    """
+
+    href = obj["@controls"][ctrl]["href"]
+    method = obj["@controls"][ctrl]["method"].lower()
+    assert method == "delete"
+    resp = client.delete(href)
+    assert resp.status_code == 204
+
+def _check_control_put_method(ctrl, client, obj)
+    """
+    Checks a PUT type control from a JSON object be it root document or an item
+    in a collection. In addition to checking the "href" attribute, also checks
+    that method, encoding and schema can be found from the control. Also
+    validates a valid sensor against the schema of the control to ensure that
+    they match. Finally checks that using the control results in the correct
+    status code of 204.
+    """
+
+    ctrl_obj = obj["@controls"][ctrl]
+    href = ctrl_obj["href"]
+    method = ctrl_obj["method"].lower()
+    encoding = ctrl_obj["encoding"].lower()
+    schema = ctrl_obj["schema"]
+    assert method == "put"
+    assert encoding == "json"
+    body = _get_user_json()
+    body["username"] = obj["username"]
+    validate(body, schema)
+    resp = client.put(href, json=body)
+    assert resp.status_code == 204
