3ebec446022982a7703caab141d925dd172c53f4
Samuli Helttunen <gwchilos@hotmail.com>
2019-07-31 19:05:17 +0300

    ApiError, iso formats for date and time, prompt recipe choice

--- Soodit/Final_api/mealplan/submit.py
+++ Soodit/Final_api/mealplan/submit.py
@@ -39,3 +39,46 @@ API_TAG_RECIPESTEP_MAPPING = {
     "step" : "step",
     "text" : "instruction"
 }
+
+class APIError(Exception):
+    """
+    used when API responds with an error code
+    """
+    def __init__(self, code, error):
+
+        self.error = json.loads(error)
+        self.code = code
+
+    def __str__(self):
+
+        return "Error {code} while accessing {uri}\nDetails:\n{msgs}".format(
+            code=self.code,
+            uri=self.error["resource_url"],
+            msg=self.error["@error"]["@messages"],
+            msgs="\n".join(self.error["@error"]["@messages"])
+        )
+
+def make_iso_format_date(value):
+
+    for form in DATE_FORMATS:
+        try:
+            date = time.strptime(value, form)
+            value = time.strftime(ISO_DATE, date)
+            break
+        except ValueError:
+            pass
+    else:
+        value = input("Type ISO format date that matches {}".format(value))
+    return value
+
+def make_iso_format_time(value):
+
+    return time.strftime(ISO_TIME, time.gmtime(value))
+
+def prompt_recipe_choice(name, hits):
+
+    print("The following recipes were found with '{}'".format(name))
+    for i, recipe in enumerate(hits, 1):
+        print("{i}: {name} ({description},{recipeyield},{cookTime},{recipeCategory},{author},{datePublished}}".format(i=i, **recipe))
+    choice = int(input("Choose artist by typing a number: "))
+    return items[choice - 1]
